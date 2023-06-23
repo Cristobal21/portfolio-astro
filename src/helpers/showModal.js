0d6641ef0a86a01307a47d33f303ee35b5be5459
Aorus <cristodel95@gmail.com>
2023-06-23 13:23:19 -0400

    Se adapta el Home.astro para pantallas grandes y se agrega menu de navevgacion

---
+++ src/helpers/showModal.js
@@ -0,0 +1,18 @@
+
+
+export const handleModal = () => {
+
+  // const icon = document.querySelector('#verMas')
+  const div = document.getElementById('divhidden')
+
+  if (div.style.display === "hidden") {
+    div.style.display = 'block'
+  } else {
+    div.style.display = 'none'
+  }
+
+}
+
+
+
+// export const handleClick = () => {} 
